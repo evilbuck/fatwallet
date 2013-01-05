9004db3475b8d025220d8e5cad8f626018234a89
me@buckrobinson.com <me@buckrobinson.com>
2013-01-05 10:34:13 -0800

    wip; - implementing browser actions via html5 notifications - stubbed out changing the popup html for the browser action

--- chrome/background.js
+++ chrome/background.js
@@ -1,8 +1,19 @@
 ;(function(){
   var tddiumUrl = 'https://api.tddium.com/cc/88b9f80b5bbe2b868e322362762e8ec1666f1f76/cctray.xml',
     // TODO: let this come from localStorage eventually
-    featurePrefix = /br\//;
-  
+    featurePrefix = /br\//,
+    branches = {};
+
+  function parseProject( projectNode ) {
+    return { name: projectNode.attr('name'),
+      lastBuildTime: projectNode.attr('lastBuildTime'),
+      activity: projectNode.attr('activity') }
+  }
+
+  // TODO: show notification; change popup html
+  function handleBranchChange(argument) {
+    // body...  
+  }
   function fetchStatus(){
     $.ajax({
       url: tddiumUrl,
@@ -12,16 +23,23 @@
         });
 
         $(featureBranches).each(function(){
-          var $branch = $(this),
-            name = $branch.attr('name');
-
           // TODO: change icon
-          console.log( $branch.attr('name'), $branch.attr('activity'), $branch.attr('lastBuildStatus') || 'N/A' );
-          webkitNotifications.createNotification(
-            'icon.png',
-            name,
-            name + ": " + $branch.attr('lastBuildStatus') || $branch.attr('activity')
-          ).show();
+          var $branch = $(this),
+            name = $branch.attr('name'),
+            notification = webkitNotifications.createNotification(
+              'icon.png',
+              name,
+              name + ": " + $branch.attr('lastBuildStatus') || $branch.attr('activity')
+            ),
+            project = parseProject( $branch );
+
+          if ( ! _.has(branches, name) ) {
+            branches[ name ] = project;
+            notification.show();
+          } else if ( ! _.isEqual(branches[name], project) ) {
+            branches[ name ] = project;
+            notification.show();
+          }
         });
       },
       dataType: 'xml'
