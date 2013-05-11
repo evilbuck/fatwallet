d5d0ab0c02af08cab096e492bc42e19d3d63970b
me@buckrobinson.com <me@buckrobinson.com>
2013-05-11 16:43:33 -0700

    wip; download button working

--- chrome/application.js
+++ chrome/application.js
@@ -1,114 +1,35 @@
-(function(){
-  var ENVIRONMENT = 'development';
-  var global = this, 
-  App = function(){
-    var $toolbar, $deals, 
-      $logo, $close_btn,
-      self = this;
-    
-    this.$toolbar = $toolbar = $(
-      '<div class="c2b-toolbar">' + 
-        '<div class="icon-logo" title="Fatwallet offers cashback here.">' +
-          '<div class="c2b-headline">Cashback Available!</div>' +
-        '</div>' +
-        '<div class="close-button" title="close"/>' +
-        '<div class="deals"/>' +
-      '</div>'
-    );
-    this.$deals = $toolbar.find('.deals');
-    $logo =  $toolbar.find('.icon-logo');
-    $logo.width(140);
-    $close_btn = $toolbar.find('.close-button');
-    $toolbar.css('opacity', 0);
+;(function($){
+  var youtuberUrl, $button;
 
-    $logo.click(function(){
-      if ( self.$('.deals .deal').size() === 1 ) {
-        return self.$('.deals .deal').click();
-      }
-      //self.toggle_deals();
-    });
-
-    $close_btn.click(function(){
-      self.destroy();
-    });
-   
-    this.get_uid();
-    this.register_listener();
-
-  };
-
-  App.prototype.register_listener = function() {
-    var self = this;
-    chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-      switch( request.call ) {
-        case "toggle_deals":
-          self.toggle_deals();
-          break;
-
-        case "build_deals":
-          $(request.data).each(function( index ){
-            var item = this;
-            $('<div class="deal ' + (index % 2 ? 'alt' : '' ) + '">' + 
-               this.v + '</div>')
-              .click(function(){
-              location.href = "http://fatwallet.com/" + item.u;
-            }).appendTo( self.$deals );
-          });
-
-          $('body').append( self.$toolbar );
-
-          if ( self.stand_down() ) {
-            self.$toolbar.css( 'opacity', 1 );
-            self.record_shown();
-          }
-
-          break;
-
-        default:
-          throw "unknown request"
-      }
-
-    });
-  };
-
-  App.prototype.toggle_deals = function() {
-    parseInt(this.$().css('opacity'), 10) < .5 ? this.$().css({ opacity: 1 }) : this.$().css('opacity', 0);
-  };
-
-  App.prototype.record_shown = function() {
-    localStorage.setItem('last_shown', new Date());
-  };
-
-  App.prototype.stand_down = function() {
-    var last_shown = localStorage.getItem('last_shown');
-    if ( ! last_shown || (new Date() - new Date(last_shown)) / 1000 / 60  > 60 ) {
-      return false;
-    }
-    return true;
-  };
-
-  App.prototype.destroy = function(){
-    return this.toggle_deals();
-    this.$().fadeOut(250, function(){
-      self.$().remove();
-    });
-    // TODO: remove the application instance and return memory
-  };
-
-  App.prototype.$ = function(selector) {
-    return selector ? $(selector, this.$toolbar) : this.$toolbar;
-  };
-
-  App.prototype.get_uid = function() {
-    var self = this;
-    chrome.extension.sendMessage({ call: "user.uid" }, function(response) {
-      self.uid = response;
-    });
-  };
-
-  new App();
-  $(function(){
-    //var fatwalletApp = new App();
-    //if (ENVIRONMENT === 'development') global.fatwalletApp = fatwalletApp;
+  youtuberUrl = 'http://localhost:3000';
+  // add a download button
+  $button = $('<button><span class="yt-uix-button-content">download audio</span></button>');
+  $button.css({
+    color: 'rgb(51, 51, 51)',
+    cursor: 'pointer',
+    border: '1px solid rgb(204, 204, 204)',
+    'border-radius': '2px',
+    'background-image': 'linear-gradient(rgb(255, 255, 255) 0px, rgb(224, 224, 224) 100%)',
+    font: 'bold 12px "Arial", "sans-serif"',
+    padding: '5px 6px'
   });
-}).call(this);
+
+  // add handler to button
+  $button.on('click.download', function(jqEvent){
+    $.ajax({
+      url: youtuberUrl + '/queue',
+      data: { video_url: location.href },
+      success: function(res){
+        console.log(res);
+        window.top.location.href = youtuberUrl + "/download/" + encodeURIComponent( res.files[0] );
+      },
+      // handle errors
+      error: function(res){
+        console.log(res);
+      },
+      dataType: 'json'
+    });
+  });
+  
+  $button.appendTo('#watch-headline-title');
+})(jQuery);
